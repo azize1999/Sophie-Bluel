54d31b32754eafbf89916d4f226ce3516302a0c7
azize1999 <azizchatti00@gmail.com>
2024-08-24 01:24:03 +0200

    setFilter started

--- FrontEnd/js/app.js
+++ FrontEnd/js/app.js
@@ -14,8 +14,7 @@ throw new Error(` Response status: ${response. status}`);
 }
 
 const json = await response.json();
-
-console.log(json); 
+ 
 
 for(let i = 0; i < json.length; i++){
     setFigure(json[i]);
@@ -33,4 +32,31 @@ const figure = document.createElement("figure");
 figure .innerHTML = `<img src=${data.imageUrl} alt=${data.title}>
 				<figcaption>${data.title}</figcaption>`;
 document.querySelector(".gallery").append(figure);            
+}
+
+async function getCategories() {
+    const url = "http://localhost:5678/api/categories";
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+        const json = await response.json();
+        console.log(json);
+         for (let i = 0; i < json.length; i++) {
+            setFilter(json[i]);
+         }
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+getCategories();
+
+
+
+function setFilter(data){
+    const div = document.createElement("div");
+div .innerHTML = `${data.name}`;
+document.querySelector(".div-ontainer").append(div); 
 }
